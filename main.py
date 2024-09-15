2566afd74d4a66d9a26b7f6e1804020d8a8c4e19
Mohammad <mhamdl2amen@gmail.com>
2024-09-15 14:43:35 +0300

    more details

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template
 import requests
+import smtplib
+my_email="mhamdl2amen@gmail.com"
+my_password="rlnhisqivnloudab"
+
+from flask import request
 API_KEY = "https://api.npoint.io/649f1724390381724336"
 response = requests.get(API_KEY)
 posts = response.json()
@@ -20,6 +25,23 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        post= "Successfully sent message."
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(from_addr=email,
+             to_addrs=my_email,
+             msg=f"Subject:New Message\n\nName:{name}\nEmail:{email}\nPhone:{phone}\nMessage:{message}")
+    return render_template("contact.html", post=post)
+        
+    
             
 if __name__ == "__main__":
     app.run(debug=True,host="localhost", port="5000")
